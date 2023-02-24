2220408beeed2c934d0d23ac4f07d5843345cbcd
Rupali Dinde <rupali.dinde@inflectionzone.com>
2023-02-24 14:29:02 +0530

    Added assessment folder

---
+++ src/routes/users/[userId]/assessments/+page.svelte
@@ -0,0 +1,174 @@
+<script>
+// @ts-nocheck
+
+  import Paginate from '$lib/components/paginate/paginate.svelte';
+  import Fa from 'svelte-fa';
+  import { faCirclePlus, faPencil, faTrash } from '@fortawesome/free-solid-svg-icons';
+//   import Navbar from '$lib/components/navbar/navbar.svelte';
+  let columns = ['Title', 'Display Code', 'Type', 'Provider', 'Created Date'];
+  let items = [
+    ['Maternity (Regular)', 'MATERNITY', 'Maternity and Neonatal', 'Rean', '01-March-2020'],
+    ['Sport Injury (Post-Op)', 'SPORTS-POST-OP', 'Post Surgery Care', 'Rean', '01-March-2020'],
+    ['Eye care (Post-Op)', 'EYE-POST-OP', 'Post Surgery Care', 'Rean', '01-March-2020'],
+    ['Spinal Physiotherapy', 'SPINAL-PHYSIO', 'Neuro Care', 'Rean', '01-March-2020'],
+    [
+      'Neonatal Special Care',
+      'NEONATAL-SPECIAL',
+      'Maternity and Neonatal',
+      'Rean',
+      '01-March-2020'
+    ],
+    ['Maternity (Regular)', 'MATERNITY', 'Maternity and Neonatal', 'Rean', '01-March-2020'],
+    [
+      'Neonatal Special Care',
+      'NEONATAL-SPECIAL',
+      'Maternity and Neonatal',
+      'Rean',
+      '01-March-2020'
+    ],
+    ['Maternity (Regular)', 'MATERNITY', 'Maternity and Neonatal', 'Rean', '01-March-2020'],
+    [
+      'Neonatal Special Care',
+      'NEONATAL-SPECIAL',
+      'Maternity and Neonatal',
+      'Rean',
+      '01-March-2020'
+    ],
+    ['Maternity (Regular)', 'MATERNITY', 'Maternity and Neonatal', 'Rean', '01-March-2020'],
+    ['Eye care (Post-Op)', 'EYE-POST-OP', 'Post Surgery Care', 'Rean', '01-March-2020'],
+    ['Spinal Physiotherapy', 'SPINAL-PHYSIO', 'Neuro Care', 'Rean', '01-March-2020'],
+    ['Eye care (Post-Op)', 'EYE-POST-OP', 'Post Surgery Care', 'Rean', '01-March-2020'],
+    ['Spinal Physiotherapy', 'SPINAL-PHYSIO', 'Neuro Care', 'Rean', '01-March-2020']
+  ];
+
+  let show = false;
+
+  let currentPage = 1;
+  let pageSize = 20;
+</script>
+<!-- <Navbar /> -->
+
+<div class=" pt-2">
+  <div class=" breadcrumbs text-lg text-[#7165E3] ml-14 mt-4">
+    <ul>
+      <li><a href="/" class="text-[#7165E3]">Home</a></li>
+      <li>Assessment explorer</li>
+    </ul>
+  </div>
+
+  <div class="flex flex-row mr-14 mt-3 ">
+    <div class="basis-1/2 justify-center items-center ">
+      <div class="relative flex items-center  " />
+    </div>
+    <div class="basis-1/2 justify-center items-center">
+      <div class="relative flex items-center">
+        <a href="/assessment/create" class="absolute right-4 lg:mr-[-18px] ">
+          <Fa icon={faCirclePlus} style="color: #5832A1" size="4x" />
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div
+    class="flex flex-row mx-6 mt-10 gap-8 lg:flex-row md:flex-row sm:flex-col min-[280px]:flex-col"
+  >
+    <div class="basis-1/2 justify-center items-center ">
+      <div class="relative flex items-center">
+        <input
+          type="text"
+          placeholder="Assessment-Template Type"
+          class="input input-bordered input-info w-full"
+        />
+      </div>
+    </div>
+    <div class="basis-1/2 justify-center items-center ">
+      <div class="relative flex items-center">
+        <input
+          type="text"
+          placeholder="Search Assessment Template By Display Code"
+          class="input input-bordered input-info w-full"
+        />
+      </div>
+    </div>
+  </div>
+
+  <div
+    class="flex flex-row mx-6 mt-4 gap-8 lg:flex-row md:flex-row sm:flex-col min-[280px]:flex-col"
+  >
+    <div class="basis-1/2 justify-center items-center ">
+      <div class="relative flex items-center">
+        <input
+          type="text"
+          placeholder="Search Assessment-Template By Title"
+          class="input input-bordered input-info w-full"
+        />
+      </div>
+    </div>
+    <div class="basis-1/2 justify-center items-center">
+      <div class="relative flex items-center  ">
+        <input
+          type="text"
+          placeholder="Search Assessment-Template By Care plan"
+          class="input input-bordered input-info w-full"
+        />
+      </div>
+    </div>
+  </div>
+  <div class="flex justify-center flex-col mx-6 mt-4 overflow-auto">
+    <div class=" overflow-y-auto h-[600px]">
+      <table class="table w-full ">
+        <!-- <div class="  w-full "> -->
+        <!-- head -->
+        <thead class="sticky top-0">
+          <tr>
+            <th />
+            {#each columns as columnHeading}
+              <th aria-sort="ascending" class="">
+                <!-- <Fa icon={faArrowDown} class=""   style="color: #5832A1 first:aria-[sort=ascending]:rotate-0" size="md" /> -->
+                {columnHeading}
+              </th>
+            {/each}
+            <th />
+            <th />
+            <th />
+          </tr><tr />
+        </thead>
+
+        <tbody class="">
+          {#each items as row, index}
+            <tr>
+              <td>{pageSize * 0 + index + 1}</td>
+
+              <td class="" />
+              <!-- </a> -->
+              <td class="" />
+              <td class="" />
+              <td class="" />
+              <td />
+              <td>
+                <a href="/users/home/careplans/id/edit"
+                  ><Fa icon={faPencil} style="color: #5832A1" size="md" /></a
+                >
+              </td>
+              <td>
+                <button>
+                  <Fa icon={faTrash} style="color: #5832A1" size="md" />
+                </button>
+              </td>
+              <!-- <td></td>   -->
+            </tr>
+          {/each}
+        </tbody>
+
+        <tfoot />
+        <!-- </div> -->
+      </table>
+    </div>
+
+    <div
+      class=" border-t flex flex-col w-full bg-[#7165E3] h-12 pt-2 rounded-b-lg mb-12 xs:flex-row items-center xs:justify-between md:mr-5 sm:mr-5 mr-8"
+    >
+      <Paginate items={items.length} />
+    </div>
+  </div>
+</div>
---
+++ src/routes/users/[userId]/assessments/[id]/edit/+page.svelte
@@ -0,0 +1,141 @@
+<script lang="ts">
+
+    import Fa from 'svelte-fa';
+	import { faMultiply } from '@fortawesome/free-solid-svg-icons';
+  
+    import Navbar from "../../../../lib/components/nav/nav.svelte"
+
+
+    let retrievedTags = '';
+    let tagsPlaceholder = "Enter a tags here..."
+
+    function handleTags(event) {
+        retrievedTags = event.detail.tags;
+    }
+
+</script>
+<Navbar />
+
+<div>
+    <div class=" breadcrumbs text-xl  text-[#7165E3] ml-14 mt-10">
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Clinical</a></li>
+            <li><a href="/">Assessment-template</a></li>
+            <li>Edit</li>
+        </ul>
+    </div>
+
+    <form method="post" class="w-full max-w-4xl  bg-[#ECE4FC] mt-6 mb-20  rounded-lg mx-auto">
+        <div class="w-full  h-14 rounded-t-lg p-3  bg-[#7165E3]">
+            <div class="ml-3 relative flex flex-row text-white text-xl">
+                Edit Assessment-template
+                <Fa icon={faMultiply} size="lg" class="absolute right-0 pr-3 " />
+            </div>
+        </div>
+
+        <div class="hidden" name="assetCode"></div>
+
+        <div class="flex items-center mb-4 mt-10 mx-16">
+            <div class="w-1/3">
+                		<!-- svelte-ignore a11y-label-has-associated-control -->
+                <label class="lable-text">Title*</label>
+            </div>
+            <div class="w-2/3">
+                <input
+                    type="text"
+                    name="name"
+                    required
+                    placeholder="Enter assessment template name here..."
+                    class="input input-bordered input-info w-full "
+                />
+            </div>
+        </div>
+
+        <div class="flex items-center my-4 mx-16">
+            <div class="w-1/3">
+              		<!-- svelte-ignore a11y-label-has-associated-control -->
+                <label class="lable-text"> Description </label>
+            </div>
+            <div class="w-2/3">
+                <textarea
+                    name="description"
+                    required
+                    class="textarea textarea-info w-full"
+                    placeholder="Enter assessment description here..."
+                />
+            </div>
+        </div>
+
+        <div class="flex items-center my-4 mx-16">
+            <div class="w-1/3">
+                <label class="lable-text" for="version"> Display code</label>
+            </div>
+            <div class="w-2/3">
+                <input name="" class="input input-bordered input-info w-full" placeholder="AssessmentTemp#cholDemo">
+            </div>
+        </div>
+        <div class="flex items-center my-4 mx-16">
+            <div class="w-1/3">
+                <label class="lable-text" for="version"> Type</label>
+            </div>
+            <div class="w-2/3">
+                <select class="select select-info w-full">
+                    <option selected>Careplan</option>
+                    <option>Auto</option>
+                    <option>Dark mode</option>
+                    <option>Light mode</option>
+                  </select>
+            </div>
+        </div>
+        <div class="flex items-center my-4 mx-16">
+            <div class="w-1/3">
+                <label class="lable-text" for="version">Provider</label>
+            </div>
+            <div class="w-2/3">
+                <input name="" class="input input-bordered input-info w-full" placeholder="Rean">
+            </div>
+        </div>
+ 
+
+        <div class="flex items-center my-4 mx-16">
+            <div class="w-1/3">
+                <label class="lable-text" for="version"> Provider assessment code </label>
+            </div>
+            <div class="w-2/3">
+                <input name="" class="input input-bordered input-info w-full" placeholder="1">
+            </div>
+        </div>
+        <div class="flex items-center my-4 mx-16">
+            <div class="w-1/3">
+                <label class="lable-text" for="version"> Servelist node childrenAt once </label>
+            </div>
+            <div class="w-2/3 ">
+                  <div class="form-control flex flex-row gap-8">
+                    <label class="label cursor-pointer">
+                      <input type="checkbox" class=" checkbox checkbox-primary" />
+                      <span class="label-text ml-1">True</span> 
+                    </label>
+                    <label class="label cursor-pointer ml-2">
+                        <input type="checkbox" class=" checkbox checkbox-primary" />
+                        <span class="label-text ml-1">False</span>   
+                    </label>
+                  </div>
+                  
+                 
+            </div>
+        </div>
+
+        <div class="flex items-center my-10 mx-16">
+            <div class="w-1/2" />
+            <div class="w-1/4 ">
+              <button type="reset" class="btn btn-outline lg:w-40 sm:w-32 ml-8 mb-10"> Reset </button>
+            </div>
+            <div class="w-1/4">
+              <button type="submit" class="btn btn-primary lg:w-40 sm:w-32 ml-8 mb-10">Submit </button>
+            </div>
+          </div>
+       
+    </form>
+
+</div>
