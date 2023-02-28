48a87fa54fa74a9f3dfdb2d325d12e4f2d619f6b
Mayuresh Raskar <rmayuresh772@gmail.com>
2023-02-28 18:27:59 +0530

    fix the node questions

--- src/routes/users/[userId]/assessments/assessment-nodes/create/multiple.choice.svelte
+++ src/routes/users/[userId]/assessments/assessment-nodes/create/multiple.choice.svelte
@@ -1,68 +1,58 @@
-<script lang = 'ts'>
-  import Fa from 'svelte-fa';
-  import { faMultiply } from '@fortawesome/free-solid-svg-icons';
-  let show = false;
-  let values = [
-    {
-      name: ''
-    }
-  ];
+<script lang="ts">
+	import Fa from 'svelte-fa';
+	import { faMultiply } from '@fortawesome/free-solid-svg-icons';
 
-   let look=false
+	let show = false;
+	let optionValueStore = [
+		{
+			name: ''
+		}
+	];
 
-  const addField = () => {
-    values = [...values, { name: '' }];
-    console.log("values-------",values.length)
-  };
- 
-  const removeField = () => {
-    values = values.slice(0, values.length - 1);
-    
-  };
+	const addOptionField = () => {
+		optionValueStore = [...optionValueStore, { name: '' }];
+	};
 
-   
+	const removeOptionField = () => {
+		optionValueStore = optionValueStore.slice(0, optionValueStore.length - 1);
+	};
 
-  const handleFocus = () => {
-    show = true;
-  };
+	const handleFocus = () => {
+		show = true;
+	};
 </script>
 
 <div class=" flex justify-center ">
-  <div class="w-[29%]  " />
-  <div class="border-2 border-solid card bg-[#ffffff] h-auto  w-[57.5%]   rounded-lg">
-    <div class=" form-control my-2  flex flex-col pl-5">
-      {#each values as v, i}
-        <div class="flex  flex-row pb-4 pt-2">
-          <span class="font-semibold">{i+1}</span>
-          <input
-            type="text"
-            class="pl-2 border-b-2 w-10/12 outline-none"
-            on:focus={handleFocus}
-            bind:value={values[i].name}
-            placeholder="name"
-          />
+	<div class="w-[29%]  " />
+	<div class="border-2 border-solid card bg-[#ffffff] h-auto w-[57.5%] rounded-lg">
+		<div class=" form-control my-2 flex flex-col pl-3">
+			{#each optionValueStore as v, i}
+				<div class="flex flex-row pt-2">
+					<span class="font-semibold text-primary-500 pr-2 pt-2">{i + 1}</span>
+					<input
+						type="text"
+						class="input w-11/12"
+						on:focus={handleFocus}
+						bind:value={optionValueStore[i].name}
+						placeholder="Add option here..."
+					/>
 
-          <button class="pl-3 pt-1.5" type="button" on:click={removeField}>
-            <Fa icon={faMultiply} size="md" />
-          </button>
-        </div>
-      {/each}
-      
-    </div>
-    <div>
-      <div class="flex  justify-start pl-5 mb-2">
-        {#if show }
-         
-          <button
-            class="hover:bg-color pl-2  btn btn-sm bg-color text-md text-[14px]"
-            type="button"
-            on:click|preventDefault={addField}>Add Option</button
-          >
-        {/if}
-      </div>
-
-    </div>
-
-  </div>
-
+					<button class="pl-2 pt-1.5" type="button" on:click={removeOptionField}>
+						<Fa icon={faMultiply} size="md" class="text-primary-500" />
+					</button>
+				</div>
+			{/each}
+		</div>
+		<div>
+			<div class="flex justify-start pl-7 mb-2">
+				{#if show}
+					<button
+						class="btn variant-ringed-primary btn-sm"
+						type="button"
+						on:click|preventDefault={addOptionField}>Add Option</button
+					>
+				{/if}
+			</div>
+		</div>
+	</div>
 </div>
