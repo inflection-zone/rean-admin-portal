0569be5aa4b2098d41d600d56b00912850973fea
shivam-inflection <102375669+shivam-inflection@users.noreply.github.com>
2023-03-17 11:57:37 +0530

    Update +server.ts

--- src/routes/api/server/api-client/+server.ts
+++ src/routes/api/server/api-client/+server.ts
@@ -6,13 +6,12 @@ import { deleteApiClient} from '../../services/api-clients';
 
 export const DELETE = async (event: RequestEvent) => {
 	const request = event.request;
-    console.log("request----------",request);
 	const data = await request.json();
 	try {
 		console.log('Inside api client server endpoints');
 		const response = await deleteApiClient(
-            data.sessionId,
-            data.apiClientId
+           			 data.sessionId,
+           			 data.apiClientId
 		);
 		return new Response(response.message);
 	} catch (err) {
