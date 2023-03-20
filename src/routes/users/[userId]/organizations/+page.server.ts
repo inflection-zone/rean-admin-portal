550916a91475d4c2d5371a44f0cb7e08ae57acf3
shivam-inflection <102375669+shivam-inflection@users.noreply.github.com>
2023-03-20 12:17:32 +0530

    Update +page.server.ts

--- src/routes/users/[userId]/organizations/+page.server.ts
+++ src/routes/users/[userId]/organizations/+page.server.ts
@@ -2,14 +2,10 @@ import type { RequestEvent } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
 import { searchOrganizations } from '../../../api/services/organizations';
-
-
 ////////////////////////////////////////////////////////////////////////////
 
 export const load: PageServerLoad = async (event: RequestEvent) => {
 	const sessionId = event.cookies.get('sessionId');
-	console.log('sessionId', sessionId);
-
 	try {
 		const response = await searchOrganizations(sessionId);
 		if (response.Status === 'failure' || response.HttpCode !== 200) {
