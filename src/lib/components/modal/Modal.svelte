58633a1eebc80220401b9afcc59c9b07ace94d56
Rupali Dinde <rupali.dinde@inflectionzone.com>
2023-04-15 11:06:44 +0530

    Formating changes

--- src/lib/components/modal/Modal.svelte
+++ src/lib/components/modal/Modal.svelte
@@ -1,52 +1,65 @@
 <script>
 	import { createEventDispatcher } from 'svelte';
-  import {showModal} from '../../store/general.store';
-	export let userName = undefined
-  export let password = undefined
+	import { showModal } from '../../store/general.store';
+	export let userName = undefined;
+	export let password = undefined;
 	let dialog; // HTMLDialogElement
 
 	$: if (dialog && $showModal) dialog.showModal();
 
 	const dispatch = createEventDispatcher();
-  const handlelSubmitClick = async (userName , password) => {
-    dispatch('submit',
-		{
+	const handlelSubmitClick = async (userName, password) => {
+		dispatch('submit', {
 			userName: userName,
-			password:password
-
+			password: password
 		});
-  };
-
+	};
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<dialog class=""
+<dialog
+	class=""
 	bind:this={dialog}
-	on:close={() => (showModal.set(false))}
+	on:close={() => showModal.set(false)}
 	on:click|self={() => dialog.close()}
 >
 	<div on:click|stopPropagation class="w-full">
-		<slot name="header"/>
+		<slot name="header" />
 		<hr />
-		<h3 class="text-primary-500 mb-6" >
-			Get api key
-			</h3>
-				<form action="?/getApiKey" method="POST" >
-					<label class="label mb-4">
-						<span class = ''>User Name</span>
-						<input class="input" name='userName' type="text" bind:value={userName} placeholder="Enter name..." />
-					</label>
-					<label class="label">
-						<span>Password</span>
-						<input class="input" type="password" name="password" bind:value={password} placeholder="Enter email address..." />
-					</label>	
-				</form>
+		<h3 class="text-primary-500 mb-6">Get api key</h3>
+		<form action="?/getApiKey" method="POST">
+			<label class="label mb-4">
+				<span class="">User Name</span>
+				<input
+					class="input"
+					name="userName"
+					type="text"
+					bind:value={userName}
+					placeholder="Enter name..."
+				/>
+			</label>
+			<label class="label">
+				<span>Password</span>
+				<input
+					class="input"
+					type="password"
+					name="password"
+					bind:value={password}
+					placeholder="Enter email address..."
+				/>
+			</label>
+		</form>
 		<slot />
 		<hr />
-	<div class="flex gap-6 mt-4">
-		<button class="btn variant-filled-error text-left " on:click={() => dialog.close()}>close</button>
-		<button class="btn variant-filled-primary text-right"  on:click={() => handlelSubmitClick(userName, password)}>submit</button>
-	</div>
+		<div class="flex gap-6 mt-4">
+			<button class="btn variant-filled-error text-left " on:click={() => dialog.close()}
+				>close</button
+			>
+			<button
+				class="btn variant-filled-primary text-right"
+				on:click={() => handlelSubmitClick(userName, password)}>submit</button
+			>
+		</div>
 	</div>
 </dialog>
 
