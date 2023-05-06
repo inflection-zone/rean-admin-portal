f7bbfebd65f6b5181fd2ad9687224766da360142
Rupali Dinde <rupali.dinde@inflectionzone.com>
2023-05-06 16:48:25 +0530

    Added api for total users, active users, age wise users and gender wise users

---
+++ src/routes/api/services/statistics.ts
@@ -0,0 +1,86 @@
+import { BACKEND_API_URL } from '$env/static/private';
+import { get_ } from './common';
+
+////////////////////////////////////////////////////////////////
+
+export const getTolalUsers = async (sessionId: string, searchParams?: any) => {
+	let searchString = '';
+	if (searchParams) {
+		const keys = Object.keys(searchParams);
+		if (keys.length > 0) {
+			searchString = '?';
+			const params = [];
+			for (const key of keys) {
+				if (searchParams[key]) {
+					const param = `${key}=${searchParams[key]}`;
+					params.push(param);
+				}
+			}
+			searchString  += params.join('&');
+		}
+	}
+	const url = BACKEND_API_URL + `/statistics/users${searchString}`;
+  console.log("url", url)
+	return await get_(sessionId, url, true);
+};
+
+export const getActiveUsers = async (sessionId: string, searchParams?: any) => {
+	let searchString = '';
+	if (searchParams) {
+		const keys = Object.keys(searchParams);
+		if (keys.length > 0) {
+			searchString = '?';
+			const params = [];
+			for (const key of keys) {
+				if (searchParams[key]) {
+					const param = `${key}=${searchParams[key]}`;
+					params.push(param);
+				}
+			}
+			searchString  += params.join('&');
+		}
+	}
+	const url = BACKEND_API_URL + `/statistics/active-users${searchString}`;
+	return await get_(sessionId, url, true);
+};
+
+export const getGenderWiseUsers = async (sessionId: string, searchParams?: any) => {
+	let searchString = '';
+	if (searchParams) {
+		const keys = Object.keys(searchParams);
+		if (keys.length > 0) {
+			searchString = '?';
+			const params = [];
+			for (const key of keys) {
+				if (searchParams[key]) {
+					const param = `${key}=${searchParams[key]}`;
+					params.push(param);
+				}
+			}
+			searchString  += params.join('&');
+		}
+	}
+	const url = BACKEND_API_URL + `/statistics/gender-wise-users${searchString}`;
+	return await get_(sessionId, url, true);
+};
+
+export const getAgeWiseUsers = async (sessionId: string, searchParams?: any) => {
+	let searchString = '';
+	if (searchParams) {
+		const keys = Object.keys(searchParams);
+		if (keys.length > 0) {
+			searchString = '?';
+			const params = [];
+			for (const key of keys) {
+				if (searchParams[key]) {
+					const param = `${key}=${searchParams[key]}`;
+					params.push(param);
+				}
+			}
+			searchString  += params.join('&');
+		}
+	}
+	const url = BACKEND_API_URL + `/statistics/age-wise-users${searchString}`;
+	return await get_(sessionId, url, true);
+};
+
---
+++ src/routes/users/[userId]/home/+page.server.ts
@@ -0,0 +1,37 @@
+import type {  RequestEvent } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { getActiveUsers, getAgeWiseUsers, getGenderWiseUsers, getTolalUsers } from '$routes/api/services/statistics';
+
+////////////////////////////////////////////////////////////////////////////
+
+export const load: PageServerLoad = async (event: RequestEvent) => {
+	const sessionId = event.cookies.get('sessionId');
+
+	try {
+    const searchParams ={}
+		const _totalUsers = await getTolalUsers(sessionId,searchParams);
+    const _activeUsers = await getActiveUsers(sessionId);
+    const _ageWiseUsers = await getAgeWiseUsers(sessionId);
+    const _genderWiseUsers = await getGenderWiseUsers(sessionId);
+
+    const totalUsers = _totalUsers.Data.TotalUsers;
+    const activeUsers = _activeUsers.Data.ActiveUsers;
+    const ageWiseUsers = _ageWiseUsers.Data.AgeWiseUsers;
+    const genderWiseUsers  = _genderWiseUsers.Data.GenderWiseUsers;
+
+    console.log("totalUsers",totalUsers);
+    console.log("activeUsers",activeUsers);
+    console.log("ageWiseUsers",ageWiseUsers);
+    console.log("genderWiseUsers",genderWiseUsers);
+
+		return {
+			totalUsers,
+      activeUsers,
+      ageWiseUsers,
+      genderWiseUsers
+		};
+
+	} catch (error) {
+		console.error(`Error retriving users data: ${error.message}`);
+	}
+};
