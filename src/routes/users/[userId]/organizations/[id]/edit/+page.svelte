b60346d13a80ea4d07f8ec9394c9ba3a650d07fe
Rupali Dinde <rupali.dinde@inflectionzone.com>
2023-06-11 15:49:25 +0530

    Ui changes for notice, notification, and organization edit page

--- src/routes/users/[userId]/organizations/[id]/edit/+page.svelte
+++ src/routes/users/[userId]/organizations/[id]/edit/+page.svelte
@@ -129,100 +129,77 @@
 	};
 </script>
 
-<main class=" mb-32">
-	<BreadCrumbs crumbs={breadCrumbs} />
+<BreadCrumbs crumbs={breadCrumbs} />
 
-	<div class=" flex justify-center mt-5 px-3 mb-10 flex-col items-center">
-		<form
-			method="post"
-			action="?/updateOrganizationAction"
-			class="w-full  bg-[#ECE4FC] lg:mt-10 md:mt-8 sm:mt-6 mt-4 lg:max-w-4xl md:max-w-xl sm:max-w-lg  rounded-lg mx-auto"
-		>
-			<div class="w-full  h-14 rounded-t-lg p-3  bg-[#7165E3]">
-				<div class="ml-3 relative flex flex-row text-white text-xl">
-					Edit Organization
-					<a href={viewRoute}>
-						<Fa icon={faMultiply} size="lg" class="absolute right-0 pr-3 mb-16 text-white " />
+<form
+	method="post"
+	action="?/updateOrganizationAction"
+	class="table-container border border-secondary-100 my-2"
+>
+	<table class="table">
+		<thead class="!variant-soft-secondary">
+			<tr>
+				<th>Edit Organization</th>
+				<th class="text-end">
+					<a href={viewRoute} class="btn btn-icon-sm -my-2 variant-soft-secondary">
+						<Fa icon={faMultiply} size="lg" />
 					</a>
-				</div>
-			</div>
-			<div class="hidden">{id}</div>
-
-			<div class="flex items-center mb-4 mt-10 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>Type</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+				</th>
+			</tr>
+		</thead>
+		<tbody class="!bg-white">
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Type</td>
+				<td>
 					<select
-						class="select w-full"
-						bind:value={type}
-						name="type"
-						placeholder="select type here..."
-					>
-						{#each oraganizationTypes as types}
-							<option>{types}</option>
-						{/each}
-					</select>
-				</div>
-			</div>
-
-			<div class="flex items-center mb-4 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>Name</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+					class="select w-full"
+					bind:value={type}
+					name="type"
+					placeholder="select type here..."
+				>
+					{#each oraganizationTypes as types}
+						<option>{types}</option>
+					{/each}
+				</select>
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Name *</td>
+				<td>
 					<input
-						type="text"
-						name="name"
-						bind:value={name}
-						placeholder="Enter name here..."
-						class="input w-full {form?.errors?.name
-							? 'border-error-300 text-error-500'
-							: 'border-primary-200 text-primary-500'}"
-					/>
-					{#if form?.errors?.name}
-						<p class="text-error-500 text-xs">{form?.errors?.name[0]}</p>
-					{/if}
-				</div>
-			</div>
-
-			<div class="flex items-center mb-4 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>Contact Number</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+					type="text"
+					name="name"
+					bind:value={name}
+					placeholder="Enter name here..."
+					class="input w-full {form?.errors?.name
+						? 'border-error-300 text-error-500'
+						: 'border-primary-200 text-primary-500'}"
+				/>
+				{#if form?.errors?.name}
+					<p class="text-error-500 text-xs">{form?.errors?.name[0]}</p>
+				{/if}
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Contact Number *</td>
+				<td>
 					<input
-						type="text"
-						name="contactPhone"
-						bind:value={contactNumber}
-						placeholder="Enter contact number here..."
-						class="input w-full max-w-md {form?.errors?.contactPhone
-							? 'border-error-300 text-error-500'
-							: 'border-primary-200 text-primary-500'}"
-					/>
-					{#if form?.errors?.contactPhone}
-						<p class="text-error-500 text-xs">{form?.errors?.contactPhone[0]}</p>
-					{/if}
-				</div>
-			</div>
-
-			<div class="flex items-center mb-4 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>Email</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+					type="text"
+					name="contactPhone"
+					bind:value={contactNumber}
+					placeholder="Enter contact number here..."
+					class="input w-full max-w-md {form?.errors?.contactPhone
+						? 'border-error-300 text-error-500'
+						: 'border-primary-200 text-primary-500'}"
+				/>
+				{#if form?.errors?.contactPhone}
+					<p class="text-error-500 text-xs">{form?.errors?.contactPhone[0]}</p>
+				{/if}
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Email *</td>
+				<td>
 					<input
 						type="text"
 						name="contactEmail"
@@ -235,66 +212,37 @@
 					{#if form?.errors?.contactEmail}
 						<p class="text-error-500 text-xs">{form?.errors?.contactEmail[0]}</p>
 					{/if}
-				</div>
-			</div>
-
-			<div class="flex items-center mb-2 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>About</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>About</td>
+				<td>
 					<textarea
 						name="about"
 						bind:value={about}
 						placeholder="Enter about here..."
-						class="textarea w-full	{form?.errors?.about
-							? 'border-error-300 text-error-500'
-							: 'border-primary-200 text-primary-500'}"
+						class="textarea"
 					/>
-					{#if form?.errors?.about}
-						<p class="text-error-500 text-xs">{form?.errors?.about[0]}</p>
-					{/if}
-				</div>
-			</div>
-
-			<div class="flex items-center mb-4 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>Operational Since</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Operational Since</td>
+				<td>
 					<input
 						type="text"
 						name="operationalSince"
 						bind:value={operationalSince}
 						placeholder="Enter operational since here..."
-						class="input w-full {form?.errors?.operationalSince
-							? 'border-error-300 text-error-500'
-							: 'border-primary-200 text-primary-500'}"
+						class="input"
 					/>
-					{#if form?.errors?.operationalSince}
-						<p class="text-error-500 text-xs">{form?.errors?.operationalSince[0]}</p>
-					{/if}
-				</div>
-			</div>
-
+				</td>
+			</tr>
 			{#each addresses as address}
 				<input hidden type="text" name="addressId" value={address.id} />
 			{/each}
-
-			<div class="flex items-center mb-4 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>Address Type</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Address Type *</td>
+				<td>
 					<select
 						name="addressType"
 						class="select w-full"
@@ -304,142 +252,89 @@
 						<option>Home</option>
 						<option>Office</option>
 					</select>
-				</div>
-			</div>
-
-			<div class="flex items-center mb-4 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>Address Line</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Address Line *</td>
+				<td>
 					<input
-						type="text"
-						name="addressLine"
-						placeholder="Enter address here..."
-						bind:value={addressLine}
-						class="input w-full {form?.errors?.addressLine
-							? 'border-error-300 text-error-500'
-							: 'border-primary-200 text-primary-500'}"
-					/>
-					{#if form?.errors?.addressLine}
-						<p class="text-error-500 text-xs">{form?.errors?.addressLine[0]}</p>
-					{/if}
-				</div>
-			</div>
-
-			<div class="flex items-center mb-4 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>City</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+					type="text"
+					name="addressLine"
+					placeholder="Enter address here..."
+					bind:value={addressLine}
+					class="input w-full {form?.errors?.addressLine
+						? 'border-error-300 text-error-500'
+						: 'border-primary-200 text-primary-500'}"
+				/>
+				{#if form?.errors?.addressLine}
+					<p class="text-error-500 text-xs">{form?.errors?.addressLine[0]}</p>
+				{/if}
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>City</td>
+				<td>
 					<input
 						type="text"
 						name="city"
 						placeholder="Enter city here..."
 						bind:value={city}
-						class="input w-1/3 {form?.errors?.contactEmail
-							? 'border-error-300 text-error-500'
-							: 'border-primary-200 text-primary-500'}"
+						class="input"
 					/>
-					{#if form?.errors?.city}
-						<p class="text-error-500 text-xs">{form?.errors?.city[0]}</p>
-					{/if}
-				</div>
-			</div>
-			<div class="flex items-center mb-4 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>District</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>District</td>
+				<td>
 					<input
 						type="text"
 						name="district"
 						placeholder="Enter district here..."
 						bind:value={district}
-						class="input w-1/3 {form?.errors?.district
-							? 'border-error-300 text-error-500'
-							: 'border-primary-200 text-primary-500'}"
+						class="input"
 					/>
-					{#if form?.errors?.district}
-						<p class="text-error-500 text-xs">{form?.errors?.district[0]}</p>
-					{/if}
-				</div>
-			</div>
-			<div class="flex items-center mb-4 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>State/Province</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>State/Province</td>
+				<td>
 					<select
-						name="state"
-						class="select w-1/2"
-						placeholder="Select state here..."
-						bind:value={state}
-					>
-						<option>Maharashtra</option>
-					</select>
-				</div>
-			</div>
-			<div class="flex items-center mb-4 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>Country</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+					name="state"
+					class="select w-1/2"
+					placeholder="Select state here..."
+					bind:value={state}
+				>
+					<option>Maharashtra</option>
+				</select>
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Country</td>
+				<td>
 					<input
-						type="text"
-						name="country"
-						placeholder="Enter country here..."
-						class="input w-1/3 "
-						bind:value={country}
-					/>
-				</div>
-			</div>
-			<div class="flex items-center mb-4 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>Zip/postal code</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+					type="text"
+					name="country"
+					placeholder="Enter country here..."
+					class="input w-1/3 "
+					bind:value={country}
+				/>
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Zip/Postal Code</td>
+				<td>
 					<input
-						type="text"
-						name="postalCode"
-						placeholder="Enter postal code or zip code here..."
-						bind:value={postalCode}
-						class="input w-1/3 {form?.errors?.postalCode
-							? 'border-error-300 text-error-500'
-							: 'border-primary-200 text-primary-500'}"
-					/>
-					{#if form?.errors?.postalCode}
-						<p class="text-error-500 text-xs">{form?.errors?.postalCode[0]}</p>
-					{/if}
-				</div>
-			</div>
-
-			<div class="flex  my-4 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>Image Resource</span>
-					</label>
-				</div>
-				<div class="flex flex-row gap-4 w-1/2 md:w-2/3 lg:w-2/3 ">
+					type="text"
+					name="postalCode"
+					placeholder="Enter postal code or zip code here..."
+					bind:value={postalCode}
+					class="input"
+				/>
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td class="align-top">Image</td>
+				<td>
 					{#if imageUrl === 'undefined'}
 						<input
 							name="fileinput"
@@ -459,48 +354,24 @@
 						/>
 					{/if}
 					<input type="hidden" name="imageResourceId" bind:value={imageResourceId} />
-				</div>
-			</div>
-
-			<div class="flex items-center mb-4 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>Is Health Facility Available</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
-					<label class="label cursor-pointer">
-						<input
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Is Health Facility Available</td>
+				<td>
+					<input
 							type="checkbox"
 							name="isHealthFacility"
 							bind:value={isHealthFacility}
 							bind:checked={isHealthFacility}
 							class="checkbox checkbox-primary border-primary-200 hover:border-primary-400 checkbox-md ml-2"
 						/>
-					</label>
-				</div>
-			</div>
-
-			<div class="flex items-center my-8 lg:mx-16 md:mx-12 mx-4 ">
-				<div class="lg:w-1/2 md:w-1/2 sm:w-1/2  w-1/3" />
-				<div class="lg:w-1/4 md:w-1/4 sm:w-1/4  w-1/3 ">
-					<button
-						type="button"
-						on:click={handleReset}
-						class="btn variant-ringed-primary text-primary-500 lg:w-40 lg:ml-8 md:ml-6 sm:ml-1 mb-10"
-					>
-						Reset</button
-					>
-				</div>
-				<div class="lg:w-1/4 md:w-1/4 sm:w-1/4 w-1/3">
-					<button
-						type="submit"
-						class="btn variant-filled-secondary lg:w-40 lg:ml-8 md:ml-6 sm:ml-2 mb-10"
-						>Submit
-					</button>
-				</div>
-			</div>
-		</form>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<div class="flex gap-2 p-2 justify-end">
+		<button type="button" on:click={handleReset} class="btn variant-soft-secondary">Reset</button>
+		<button type="submit" class="btn variant-filled-secondary">Submit</button>
 	</div>
-</main>
+</form>
