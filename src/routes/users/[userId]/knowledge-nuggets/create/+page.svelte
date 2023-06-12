bc286f97f8fa55e9acdb12eecbad76f94fe16abf
Rupali Dinde <rupali.dinde@inflectionzone.com>
2023-06-12 12:29:25 +0530

    Ui changes for client, knowedge nugget,  lab record create page

--- src/routes/users/[userId]/knowledge-nuggets/create/+page.svelte
+++ src/routes/users/[userId]/knowledge-nuggets/create/+page.svelte
@@ -25,139 +25,82 @@
 	];
 </script>
 
-<main class="h-screen mb-10">
-	<BreadCrumbs crumbs={breadCrumbs} />
+<BreadCrumbs crumbs={breadCrumbs} />
 
-	<div class="">
-		<form
-			method="post"
-			action="?/createKnowledgeNuggetAction"
-			class="w-full  bg-tertiary-500 lg:mt-10 md:mt-8 sm:mt-6 mb-10 mt-4 lg:max-w-4xl md:max-w-xl sm:max-w-lg  rounded-lg mx-auto"
-		>
-			<div class="w-full  h-14 rounded-t-lg p-3  bg-[#7165E3]">
-				<div class="ml-3 relative flex  text-white text-xl">
-					Create Knowledge Nugget
-					<a href="/">
-						<Fa
-							icon={faMultiply}
-							size="lg"
-							class="absolute right-0 lg:pr-3 md:pr-2 pr-0 text-white"
-						/>
+<form
+	method="post"
+	action="?/createKnowledgeNuggetAction"
+	class="table-container border border-secondary-100 my-2"
+>
+	<table class="table">
+		<thead class="!variant-soft-secondary">
+			<tr>
+				<th>Create Knowledge Nugget</th>
+				<th class="text-end">
+					<a href={knowledgeNuggetsRoute} class="btn btn-icon-sm -my-2 variant-soft-secondary">
+						<Fa icon={faMultiply} size="lg" />
 					</a>
-				</div>
-			</div>
-
-			<div class="flex items-center mb-4 mt-10 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>Name *</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+				</th>
+			</tr>
+		</thead>
+		<tbody class="!bg-white">
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Name *</td>
+				<td>
 					<input
-						type="text"
-						name="topicName"
-						required
-						placeholder="Enter name here..."
-						class="input w-full {form?.errors?.topicName
-							? 'border-error-300 text-error-500'
-							: 'border-primary-200 text-primary-500'}"
-						value={form?.data?.topicName ?? ''}
-					/>
-					{#if form?.errors?.topicName}
-						<p class="text-error-500 text-xs">{form?.errors?.topicName[0]}</p>
-					{/if}
-				</div>
-			</div>
-
-			<div class="flex  mb-2 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>Brief Information</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+					type="text"
+					name="topicName"
+					required
+					placeholder="Enter name here..."
+					class="input w-full {form?.errors?.topicName
+						? 'border-error-300 text-error-500'
+						: 'border-primary-200 text-primary-500'}"
+				/>
+				{#if form?.errors?.topicName}
+					<p class="text-error-500 text-xs">{form?.errors?.topicName[0]}</p>
+				{/if}
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td class="align-top">Brief Information</td>
+				<td>
 					<textarea
 						name="briefInformation"
-						placeholder="Enter brief information here..."
-						class="textarea w-full {form?.errors?.briefInformation
-							? 'border-error-300 text-error-500'
-							: 'border-primary-200 text-primary-500'}"
+						placeholder="Enter  brief information here..."
+						class="textarea"
 					/>
-					{#if form?.errors?.briefInformation}
-						<p class="text-error-500 text-xs">{form?.errors?.briefInformation[0]}</p>
-					{/if}
-				</div>
-			</div>
-
-			<div class="flex  mb-2 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>Detailed Information</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td class="align-top">Detailed Information</td>
+				<td>
 					<textarea
 						name="detailedInformation"
 						placeholder="Enter detailed information here..."
-						class="textarea w-full {form?.errors?.detailedInformation
-							? 'border-error-300 text-error-500'
-							: 'border-primary-200 text-primary-500'}"
+						class="textarea"
 					/>
-					{#if form?.errors?.detailedInformation}
-						<p class="text-error-500 text-xs">{form?.errors?.detailedInformation[0]}</p>
-					{/if}
-				</div>
-			</div>
-
-			<div class="flex items-center mb-4 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>Additional Resource</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Additional Resources</td>
+				<td>
 					<input
 						type="text"
 						name="additionalResources"
 						class="input w-full"
 						placeholder="Enter additional resource here..."
 					/>
-					{#if form?.errors?.additionalResources}
-						<p class="text-error-500 text-xs">{form?.errors?.additionalResources[0]}</p>
-					{/if}
-				</div>
-			</div>
-
-			<div class="flex  mb-4 lg:mx-16 md:mx-12 mx-10">
-				<div class="w-1/2 md:w-1/3 lg:w-1/3 ">
-					<!-- svelte-ignore a11y-label-has-associated-control -->
-					<label class="label">
-						<span>Tags</span>
-					</label>
-				</div>
-				<div class="w-1/2 md:w-2/3 lg:w-2/3">
-					<InputChip
-						chips="variant-filled-error rounded-2xl"
-						name="tags"
-						placeholder={tagsPlaceholder}
-					/>
-					{#if form?.errors?.tags}
-						<p class="text-error-500 text-xs">{form?.errors?.tags[0]}</p>
-					{/if}
-				</div>
-			</div>
-
-			<div class="flex items-center mt-7 lg:mx-16 md:mx-12 mr-10">
-				<div class="w-3/4" />
-				<div class="w-1/4 ">
-					<button type="submit" class="btn variant-filled-secondary w-full mb-10 "> Submit </button>
-				</div>
-			</div>
-		</form>
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td class="align-top">Tags</td>
+				<td>
+					<InputChip chips="variant-filled-error rounded-2xl" name="tags"/>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<div class="flex p-2 justify-end">
+		<button type="submit" class="btn variant-filled-secondary">Submit</button>
 	</div>
-</main>
+</form>
