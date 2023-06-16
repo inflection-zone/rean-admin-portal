04b73d78ba6802b1dc2320d8bb6a0b59c5e677f7
Rupali Dinde <rupali.dinde@inflectionzone.com>
2023-06-16 13:50:44 +0530

    Implementation of  article asset

---
+++ src/routes/users/[userId]/careplan/assets/articles/create/+page.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import BreadCrumbs from '$lib/components/breadcrumbs/breadcrums.svelte';
+	import { InputChip } from '@skeletonlabs/skeleton';
+	import Icon from '@iconify/svelte';
+
+	/////////////////////////////////////////////////////////////////////////////////
+
+	export let form;
+	const userId = $page.params.userId;
+	const assetRoute = `/users/${userId}/assets`;
+	const createRoute = `/users/${userId}/assets/articles/create`;
+	const articleRoute = `/users/${userId}/assets/articles/create`;
+
+	const breadCrumbs = [
+		{
+			name: 'Assets',
+			path: assetRoute
+		},
+		{
+			name: 'Article',
+			path: articleRoute
+		},
+		{
+			name: 'Create',
+			path: createRoute
+		}
+	];
+</script>
+
+<BreadCrumbs crumbs={breadCrumbs} />
+
+<form
+	method="post"
+	action="?/createArticleAction"
+	class="table-container border border-secondary-100 my-2"
+>
+	<table class="table">
+		<thead class="!variant-soft-secondary">
+			<tr>
+				<th>Create Article</th>
+				<th class="text-end">
+					<a href={assetRoute} class="btn p-2 -my-2 variant-soft-secondary">
+						<Icon icon="material-symbols:close-rounded" class="text-lg" />
+					</a>
+				</th>
+			</tr>
+		</thead>
+		<tbody class="!bg-white">
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Name *</td>
+				<td>
+					<input
+						type="text"
+						required
+						placeholder="Enter article name here..."
+						class="input {form?.errors?.name ? 'border-error-300 text-error-500' : ''}"
+						name="name"
+					/>
+					{#if form?.errors?.name}
+						<p class="text-error-500 text-xs">{form?.errors?.name[0]}</p>
+					{/if}
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td class="align-top">Summary</td>
+				<td>
+					<textarea name="summary" class="textarea" placeholder="Enter article summary here..." />
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td class="align-top">Url</td>
+				<td>
+					<input type="url" placeholder="Enter url here..." class="input" name="pathUrl" />
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Tags</td>
+				<td>
+					<InputChip chips="variant-filled-error rounded-2xl" name="tags" />
+				</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Version</td>
+				<td>
+					<input type="text" name="version" class="input" placeholder="V 1.0" />
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<div class="flex p-2 justify-end">
+		<button type="submit" class="btn variant-filled-secondary">Submit</button>
+	</div>
+</form>
