e69da1216a57248dbf72198be237bb3e769c56b1
Rupali Dinde <rupali.dinde@inflectionzone.com>
2023-06-19 14:34:59 +0530

    Implementation of Infographics asset

---
+++ src/routes/users/[userId]/careplan/assets/infographics/[id]/view/+page.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+	import type { PageServerData } from './$types';
+	import { onMount } from 'svelte';
+	import { show } from '$lib/utils/message.utils';
+	import { LocalStorageUtils } from '$lib/utils/local.storage.utils';
+	import { page } from '$app/stores';
+	import BreadCrumbs from '$lib/components/breadcrumbs/breadcrums.svelte';
+	import Icon from '@iconify/svelte';
+
+	//////////////////////////////////////////////////////////////////////////////////
+
+	const userId = $page.params.userId;
+	const infographicsId = $page.params.id;
+	const assetRoute = `/users/${userId}/careplan/assets`;
+	const editRoute = `/users/${userId}/careplan/assets/infographics/${infographicsId}/edit`;
+	const viewRoute = `/users/${userId}/careplan/assets/infographics/${infographicsId}/view`;
+	const infographicsRoute = `/users/${userId}/careplan/assets/infographics/create`;
+
+	export let data: PageServerData;
+	let assetCode = data.infographics.AssetCode;
+	let name = data.infographics.Name;
+	let description = data.infographics.Description;
+	let pathUrl = data.infographics.Url;
+	let tags = data.infographics.Tags;
+	let version = data.infographics.Version;
+
+	onMount(() => {
+		show(data);
+		LocalStorageUtils.removeItem('prevUrl');
+	});
+
+	const breadCrumbs = [
+		{
+			name: 'Assets',
+			path: assetRoute
+		},
+		{
+			name: 'Infographics',
+			path: infographicsRoute
+		},
+		{
+			name: 'View',
+			path: viewRoute
+		}
+	];
+</script>
+
+<BreadCrumbs crumbs={breadCrumbs} />
+
+<div class="flex flex-wrap gap-2">
+	<a href={editRoute} class="btn variant-filled-secondary ml-auto">
+		<Icon icon="material-symbols:edit-outline" />
+		<span>Edit</span>
+	</a>
+</div>
+
+<div class="table-container border border-secondary-100 my-2">
+	<table class="table">
+		<thead class="!variant-soft-secondary">
+			<tr>
+				<th>View Infographics</th>
+				<th class="text-end">
+					<a href={assetRoute} class="btn p-2 -my-2 variant-soft-secondary">
+						<Icon icon="material-symbols:close-rounded" class="text-lg" />
+					</a>
+				</th>
+			</tr>
+		</thead>
+		<tbody class="!bg-white">
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Asset Code</td>
+				<td>{assetCode}</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Name</td>
+				<td>{name}</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td class="align-top">Description</td>
+				<td>{description}</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Url</td>
+				<td>{pathUrl}</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Tags</td>
+				<td>{tags}</td>
+			</tr>
+			<tr class="!border-b !border-b-secondary-100">
+				<td>Version</td>
+				<td>{version}</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
