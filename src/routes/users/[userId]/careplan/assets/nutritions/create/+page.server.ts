303c2082fd7c9794364d6ce02f91fcca82e68888
Rupali Dinde <rupali.dinde@inflectionzone.com>
2023-06-20 08:54:03 +0530

    Added nutrition asset

---
+++ src/routes/users/[userId]/careplan/assets/nutritions/create/+page.server.ts
@@ -0,0 +1,67 @@
+import { redirect } from 'sveltekit-flash-message/server';
+import type { RequestEvent } from '@sveltejs/kit';
+import { errorMessage, successMessage } from '$lib/utils/message.utils';
+import { zfd } from 'zod-form-data';
+import { z } from 'zod';
+import { createNutrition } from '$routes/api/services/careplan/assets/nutrition';
+
+/////////////////////////////////////////////////////////////////////////
+
+const createNutritionSchema = zfd.formData({
+	name: z.string().max(128),
+	description: z.string().optional(),
+	tags: z.array(z.string()).optional(),
+	version: z.string().optional()
+});
+
+export const actions = {
+	createNutritionAction: async (event: RequestEvent) => {
+		const request = event.request;
+		const userId = event.params.userId;
+		const sessionId = event.cookies.get('sessionId');
+		const data = await request.formData();
+		const formData = Object.fromEntries(data);
+		const tags = data.has('tags') ? data.getAll('tags') : [];
+		const formDataValue = { ...formData, tags: tags };
+
+		type NutritionSchema = z.infer<typeof createNutritionSchema>;
+
+		let result: NutritionSchema = {};
+		try {
+			result = createNutritionSchema.parse(formDataValue);
+			console.log('result', result);
+		} catch (err: any) {
+			const { fieldErrors: errors } = err.flatten();
+			console.log(errors);
+			const { ...rest } = formData;
+			return {
+				data: rest,
+				errors
+			};
+		}
+
+		const response = await createNutrition(
+			sessionId,
+			result.name,
+			result.description,
+			result.tags,
+			result.version
+		);
+		const id = response.Data.id;
+		console.log(response);
+		if (response.Status === 'failure' || response.HttpCode !== 201) {
+			throw redirect(
+				303,
+				`/users/${userId}/careplan/assets`,
+				errorMessage(response.Message),
+				event
+			);
+		}
+		throw redirect(
+			303,
+			`/users/${userId}/careplan/assets/nutritions/${id}/view`,
+			successMessage(`Nutrition plan created successful!`),
+			event
+		);
+	}
+};
