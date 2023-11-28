0eee541e26a17c68be389f10e7f88f81c595b4db
Nisha-inflectionzone <nisha.londhe@inflectionzone.com>
2023-11-28 15:21:27 +0530

    bg color removal for upload appointment pdf

--- src/routes/open/appointment.uploads/+page.svelte
+++ src/routes/open/appointment.uploads/+page.svelte
@@ -1,47 +1,61 @@
 <script lang="ts">
 	import { enhance } from "$app/forms";
   import { page } from '$app/stores';
-//   import BreadCrumbs from '$lib/components/breadcrumbs/breadcrums.svelte';
 
-//   const userId = $page.params.userId;
-
-//   const importRoute = `/users/${userId}/assessment-templates/import`;
-// 	const assessmentsRoutes = `/users/${userId}/assessment-templates`;
-// const breadCrumbs = [
-// 		{ name: 'Assessments', path: assessmentsRoutes },
-// 		{ name: 'Import', path: importRoute }
-// 	];
 </script>
-
-<!-- <BreadCrumbs crumbs={breadCrumbs} /> -->
-<form
-	method="post"
-	action="?/uploadAppoinment"
-  use:enhance enctype="multipart/form-data"
-	class="table-container my-2 border border-secondary-100 dark:!border-surface-700 "
+<svelte:head>
+  <title>REAN careplans</title>
+  <meta name="description" content="REAN careplans" />
+</svelte:head>
+<body>
+  <div class="nav h-12 w-full " />
+  <div class="w-full h-full" id="background-image">
+    <div class=" h-full w-full">
+      <!-- <div class="h-10 w-screen shadow-xl mb-4 bg-[#7165E3]" /> -->
+      <div class="h-full w-full px-3">
+        <div class=" flex justify-center flex-col items-center">
+          <img
+            class="ct-image w-36 mt-14 mb-7"
+            alt="logo"
+            src="https://www.reanfoundation.org/wp-content/uploads/2021/10/REAN-Foundation-brand-Logo.png"
+          />
+          <form
+            method="post"
+            action="?/uploadAppoinment"
+            use:enhance enctype="multipart/form-data"
+            class="table-container mt-4 my-2 dark:!border-surface-700 "
 >
-<table class="table">
-  <thead class="!variant-soft-secondary">
-    <tr class="content-center">
-      <th class="text-center">Upload Appointment follow-up pdf</th>
-    </tr>
-  </thead>
-  <tbody class="!bg-white dark:!bg-inherit">
-    <tr class="!border-b !border-b-secondary-100 dark:!border-b-surface-700">
-      <td class="text-center">
-      <input 
-      type="file" 
-      name="name" 
-      required
-      class="true input w-[50%]"
-      />
-      </td>
-    </tr>
-    
-    <tr class="!border-b !border-b-secondary-100 dark:!border-b-surface-700">
-      <td class="text-center">
-        <button type="submit" class="btn variant-filled-secondary">Upload</button>
-      </td>
-    </tr>
-   </tbody>
-</form>
+            <table class="table w-2/3 mx-auto">
+              <thead class="!variant-soft-secondary">
+              <tr class="content-center">
+              <th class="text-center font-bold">Upload Appointment follow-up pdf</th>
+              </tr>
+              </thead>
+              <tbody class="!bg-white dark:!bg-inherit">
+              <tr class="!border-b !border-b-secondary-100 dark:!border-b-surface-700">
+               <td class="text-center">
+                <input
+                 type="file"
+                 name="name"
+                 required
+                 class="true input w-[50%]"
+                 />
+               </td>
+             </tr>
+             <tr class="!border-b !border-b-secondary-100 dark:!border-b-surface-700">
+             <td class="text-center">
+             <button type="submit" class="btn variant-filled-secondary">Upload</button>
+             </td>
+             </tr>
+           </tbody>
+         </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer class="w-full fixed bottom-0 bg-primary-500 text-center p-2">
+    <a href="https://reanfoundation.org" class="!text-white">&#xa9; 2022 REAN Foundation</a>
+  </footer>
+</body>
+
+
