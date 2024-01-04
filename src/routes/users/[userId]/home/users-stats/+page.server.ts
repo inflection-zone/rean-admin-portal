239dbcad4aa0fca50fac8aaecf6b495cdbd9369e
Dattatraya-Londhe <londhedn@gmail.com>
2024-01-04 14:49:57 +0530

    Updated daily statistics property - Statistics to DashboardStats

--- src/routes/users/[userId]/home/users-stats/+page.server.ts
+++ src/routes/users/[userId]/home/users-stats/+page.server.ts
@@ -11,16 +11,16 @@ export const load: PageServerLoad = async (event: RequestEvent) => {
         throw error(404, 'Daily user statistics data not found');
     }
 
-    const overallUsersData = response.Data.DailyStatistics.Statistics.UserStatistics.UsersCountStats;
-    const ageWiseUsers = response.Data.DailyStatistics.Statistics.UserStatistics.AgeWiseUsers;
-    const genderWiseUsers = response.Data.DailyStatistics.Statistics.UserStatistics.GenderWiseUsers;
-    const maritalStatusWiseUsers = response.Data.DailyStatistics.Statistics.UserStatistics.MaritalStatusWiseUsers;
-    const countryWiseUsers = response.Data.DailyStatistics.Statistics.UserStatistics.CountryWiseUsers;
-    const majorAilment = response.Data.DailyStatistics.Statistics.UserStatistics.MajorAilmentDistribution;
-    const addictionDistribution = response.Data.DailyStatistics.Statistics.UserStatistics.AddictionDistribution;
-    const deviceDetailWiseUsers = response.Data.DailyStatistics.Statistics.UserStatistics.DeviceDetailWiseUsers;
+    const overallUsersData = response.Data.DailyStatistics.DashboardStats.UserStatistics.UsersCountStats;
+    const ageWiseUsers = response.Data.DailyStatistics.DashboardStats.UserStatistics.AgeWiseUsers;
+    const genderWiseUsers = response.Data.DailyStatistics.DashboardStats.UserStatistics.GenderWiseUsers;
+    const maritalStatusWiseUsers = response.Data.DailyStatistics.DashboardStats.UserStatistics.MaritalStatusWiseUsers;
+    const countryWiseUsers = response.Data.DailyStatistics.DashboardStats.UserStatistics.CountryWiseUsers;
+    const majorAilment = response.Data.DailyStatistics.DashboardStats.UserStatistics.MajorAilmentDistribution;
+    const addictionDistribution = response.Data.DailyStatistics.DashboardStats.UserStatistics.AddictionDistribution;
+    const deviceDetailWiseUsers = response.Data.DailyStatistics.DashboardStats.UserStatistics.DeviceDetailWiseUsers;
     const years = [];
-    const yearWiseUserCount = response.Data.DailyStatistics.Statistics.UserStatistics.YearWiseUserCount;
+    const yearWiseUserCount = response.Data.DailyStatistics.DashboardStats.UserStatistics.YearWiseUserCount;
     yearWiseUserCount.forEach((value) => {
         years.push({
             year: value.Year
