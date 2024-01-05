1d9963ba1e3c7ab041208f754e6df65ffa0598bd
rf-opssupport <rf-opssupport@kansocloud.com>
2024-01-05 16:45:47 +0530

    Modified workflows

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#prod
+aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/rean-admin-portal/.env ./.env
+aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/rean-admin-portal/constants.ts /app/src/lib
+
+#aha-prod
+aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/rean-admin-portal/.env ./.env
+aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/rean-admin-portal/constants.ts /app/src/lib
+
+cd /app
+# Add any other scripts here...
+# Start the service
+# npm run start
+node index.js
