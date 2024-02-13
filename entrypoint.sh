20f52ddf745864b91425aaed1e7029e9af6407bd
Priyanka-Inflectionzone <priyanka.kale@inflectionzone.com>
2024-02-13 20:16:16 +0530

    Modified entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #prod
-aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/rean-admin-portal/.env ./.env
-aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/rean-admin-portal/constants.ts /app/src/lib
+aws s3 cp s3://reancare-configs/rean-admin-portal/.env ./.env
+aws s3 cp s3://reancare-configs/rean-admin-portal/constants.ts /app/src/lib
 
-#aha-prod
-aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/rean-admin-portal/.env ./.env
-aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/rean-admin-portal/constants.ts /app/src/lib
+# #aha-prod
+# aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/rean-admin-portal/.env ./.env
+# aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/rean-admin-portal/constants.ts /app/src/lib
 
 cd /app/build
 # Add any other scripts here...
